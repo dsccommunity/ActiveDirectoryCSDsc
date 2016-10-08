19f62d7225b988b7cf305cf737cb66d607ec78a3
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-10-08 16:00:57 +1300

    Add uninstall test to Integration tests. Remove install ADCS from AppVeyor because it won't work

--- Tests/Integration/MSFT_xAdcsCertificationAuthority.Tests.ps1
+++ Tests/Integration/MSFT_xAdcsCertificationAuthority.Tests.ps1
@@ -1,11 +1,16 @@
+# Suppress this PSSA message because we need to allow credentials to be
+# set when running the tests.
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '')]
+param ()
+
 # IMPORTANT: To run these tests requires a local Administrator account to be
 # available on the machine running the tests that can be used to install the
 # ADCS component being tested. Please change the following values to the
 # credentials that are set up for this purpose.
-# If running the tests on AppVeyor, an account will automatically
-# be added with these credentials by the AppVeyor.yml script.
+# These tests can not be run on AppVeyor because installing ADCS components
+# requires restart of the server which is not supported.
 $script:adminUsername   = 'AdcsAdmin'
-$script:adminPassword   = 'P4ssword1!'
+$script:adminPassword   = 'NotARealPassword!'
 $script:DSCModuleName   = 'xAdcsDeployment'
 $script:DSCResourceName = 'MSFT_xAdcsCertificationAuthority'
 
@@ -40,10 +45,10 @@ try
     }
 
     #region Integration Tests
-    $ConfigFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:DSCResourceName).config.ps1"
+    $ConfigFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:DSCResourceName)_Install.config.ps1"
     . $ConfigFile -Verbose -ErrorAction Stop
 
-    Describe "$($script:DSCResourceName)_Integration" {
+    Describe "$($script:DSCResourceName)_Install_Integration" {
         Context 'Install ADCS Certification Authority' {
             #region DEFAULT TESTS
             It 'Should compile without throwing' {
@@ -60,7 +65,7 @@ try
                         )
                     }
 
-                    & "$($script:DSCResourceName)_Config" `
+                    & "$($script:DSCResourceName)_Install_Config" `
                         -OutputPath $TestEnvironment.WorkingFolder `
                         -ConfigurationData $ConfigData
                     Start-DscConfiguration -Path $TestEnvironment.WorkingFolder `
@@ -78,6 +83,48 @@ try
                     $_.ConfigurationName -eq "$($script:DSCResourceName)_Config"
                 }
                 $current.Ensure           | Should Be 'Present'
+            }
+        }
+    }
+
+    $ConfigFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:DSCResourceName)_Uninstall.config.ps1"
+    . $ConfigFile -Verbose -ErrorAction Stop
+
+    Describe "$($script:DSCResourceName)_Uninstall_Integration" {
+        Context 'Uninstall ADCS Certification Authority' {
+            #region DEFAULT TESTS
+            It 'Should compile without throwing' {
+                {
+                    $secureAdminPassword = ConvertTo-SecureString -String $script:adminPassword -AsPlainText -Force
+                    $adminCred = New-Object -TypeName System.Management.Automation.PSCredential `
+                        -ArgumentList ($script:adminUsername,$secureAdminPassword)
+                    $ConfigData = @{
+                        AllNodes = @(
+                            @{
+                                NodeName    = 'localhost'
+                                AdminCred   = $AdminCred
+                            }
+                        )
+                    }
+
+                    & "$($script:DSCResourceName)_Uninstall_Config" `
+                        -OutputPath $TestEnvironment.WorkingFolder `
+                        -ConfigurationData $ConfigData
+                    Start-DscConfiguration -Path $TestEnvironment.WorkingFolder `
+                        -ComputerName localhost -Wait -Verbose -Force
+                } | Should not throw
+            }
+
+            It 'should be able to call Get-DscConfiguration without throwing' {
+                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should Not throw
+            }
+            #endregion
+
+            It 'Should have set the resource and all the parameters should match' {
+                $current = Get-DscConfiguration | Where-Object {
+                    $_.ConfigurationName -eq "$($script:DSCResourceName)_Config"
+                }
+                $current.Ensure           | Should Be 'Absent'
             }
         }
     }
--- Tests/Integration/MSFT_xAdcsCertificationAuthority_Uninstall.config.1.ps1
+++ Tests/Integration/MSFT_xAdcsCertificationAuthority_Uninstall.config.1.ps1
@@ -1,11 +1,11 @@
-configuration MSFT_xADCSCertificationAuthority_Config {
+configuration MSFT_xADCSCertificationAuthority_Uninstall_Config {
     Import-DscResource -ModuleName xAdcsDeployment
 
     node localhost {
         xADCSCertificationAuthority Integration_Test {
             CAType     = 'StandaloneRootCA'
             Credential = $Node.AdminCred
-            Ensure     = 'Present'
+            Ensure     = 'Absent'
         }
     }
 }
