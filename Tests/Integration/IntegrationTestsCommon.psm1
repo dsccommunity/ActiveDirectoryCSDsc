38acb82d93a468e53e069da7dcc75c418ca48c3e
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-10-08 15:49:27 +1300

    Added integration tests for xADCSCA

---
+++ Tests/Integration/IntegrationTestsCommon.psm1
@@ -0,0 +1,35 @@
+﻿<#
+    .SYNOPSIS
+        Tests if a specific Server Feature is installed on this OS.
+
+    .OUTPUTS
+        False if this is a non-Windows Server OS.
+        True if a specific Server feature is installed, otherwise False is returned.
+
+#>
+function Test-WindowsFeature
+{
+    [CmdletBinding()]
+    [OutputType([String])]
+    param
+    (
+        [String] $Name
+    )
+
+    # Ensure that the tests can be performed on this computer
+    $ProductType = (Get-CimInstance Win32_OperatingSystem).ProductType
+    if ($ProductType -ne 3)
+    {
+        # Unsupported OS type for testing
+        Write-Verbose -Message "Integration tests cannot be run on this operating system." -Verbose
+        Return $false
+    }
+    # Server OS
+    $InstalledFeatures = Get-WindowsFeature | Where-Object -Property Installed -EQ $True
+    if ($InstalledFeatures -notcontains $Name)
+    {
+        Write-Verbose -Message "Integration tests cannot be run because $Name is not installed." -Verbose
+        Return $false
+    }
+    Return $True
+} # end function Test-WindowsFeature
