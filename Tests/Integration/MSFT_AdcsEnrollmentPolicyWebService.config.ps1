4e8c2ec84450cb95b94d8aa896dcc47c8b9cde59
Daniel Scott-Raynsford <plagueho@gmail.com>
2019-09-15 11:30:53 +1200

    Another attempt at correcting Integration tests

--- Tests/Integration/MSFT_AdcsEnrollmentPolicyWebService.config.ps1
+++ Tests/Integration/MSFT_AdcsEnrollmentPolicyWebService.config.ps1
@@ -1,8 +1,11 @@
-configuration MSFT_AdcsEnrollmentPolicyWebService_Config {
+configuration MSFT_AdcsEnrollmentPolicyWebService_Config
+{
     Import-DscResource -ModuleName ActiveDirectoryCSDsc
 
-    node localhost {
-        AdcsEnrollmentPolicyWebService Integration_Test {
+    node localhost
+    {
+        AdcsEnrollmentPolicyWebService Integration_Test
+        {
             AuthenticationType = $Node.AuthenticationType
             SslCertThumbprint  = $Node.SslCertThumbprint
             Credential         = $Node.Credential
