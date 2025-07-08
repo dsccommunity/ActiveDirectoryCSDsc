87194e9adc9515cdb1df85bcd210f81f2676495e
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-07-08 18:26:23 +0100

    Clean up tests

--- tests/Unit/Private/Get-CaAiaUriList.Tests.ps1
+++ tests/Unit/Private/Get-CaAiaUriList.Tests.ps1
@@ -49,240 +49,200 @@ AfterAll {
 }
 
 Describe 'Get-CaAiaUriList' -Tag 'Private' {
-    Context 'When ExtensionType is AddToCertificateAia and there are only AddToCertificateOcsp URIs' {
-        BeforeAll {
-            Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
-                return @(
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $false
-                        AddToCertificateOcsp = $true
-                        Uri                  = 'http://primary-ocsp-responder/ocsp'
-                    }
-                )
+    Context 'When ExtensionType is AddToCertificateAia' {
+        Context 'When there are only AddToCertificateOcsp URIs' {
+            BeforeAll {
+                Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
+                    return @(
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $false
+                            AddToCertificateOcsp = $true
+                            Uri                  = 'http://primary-ocsp-responder/ocsp'
+                        }
+                    )
+                }
+            }
+
+            It 'Should return the correct result' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+
+                    { $script:result = Get-CaAiaUriList -ExtensionType 'AddToCertificateAia' } | Should -Not -Throw
+
+                    $result | Should -BeNullOrEmpty
+                }
+
+                Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
             }
         }
 
-        It 'Should return the correct result' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                { $script:result = Get-CaAiaUriList -ExtensionType 'AddToCertificateAia' } | Should -Not -Throw
-
-                $result | Should -BeNullOrEmpty
+        Context 'When there is one AddToCertificateAia URI and one AddToCertificateOcsp URI' {
+            BeforeAll {
+                Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
+                    return @(
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $false
+                            AddToCertificateOcsp = $true
+                            Uri                  = 'http://primary-ocsp-responder/ocsp'
+                        },
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $true
+                            AddToCertificateOcsp = $false
+                            Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
+                        }
+                    )
+                }
             }
 
-            Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
+            It 'Should return the correct result' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+
+                    { $script:result = Get-CaAiaUriList -ExtensionType 'AddToCertificateAia' } | Should -Not -Throw
+                    $result | Should -BeExactly 'http://primary/Certs/<CATruncatedName>.cer'
+                }
+
+                Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
+            }
+        }
+
+        Context 'When there is two AddToCertificateAia URI and one AddToCertificateOcsp URIs' {
+            BeforeAll {
+                Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
+                    return @(
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $false
+                            AddToCertificateOcsp = $true
+                            Uri                  = 'http://primary-ocsp-responder/ocsp'
+                        },
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $true
+                            AddToCertificateOcsp = $false
+                            Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
+                        },
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $true
+                            AddToCertificateOcsp = $false
+                            Uri                  = 'http://secondary/Certs/<CATruncatedName>.cer'
+                        }
+                    )
+                }
+            }
+
+            It 'Should not throw an exception' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+
+                }
+            }
+
+            It 'Should return the correct result' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+                    { $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateAia' } | Should -Not -Throw
+
+                    $getCaAiaUriListResult[0] | Should -BeExactly 'http://primary/Certs/<CATruncatedName>.cer'
+                    $getCaAiaUriListResult[1] | Should -BeExactly 'http://secondary/Certs/<CATruncatedName>.cer'
+                }
+
+                Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
+            }
+        }
+
+        Context 'When there are only AddToCertificateAia URIs' {
+            BeforeAll {
+                Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
+                    return @(
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $true
+                            AddToCertificateOcsp = $false
+                            Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
+                        }
+                    )
+                }
+            }
+
+            It 'Should return the correct result' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+
+                    { $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateOcsp' } | Should -Not -Throw
+
+                    $getCaAiaUriListResult | Should -BeNullOrEmpty
+                }
+
+                Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
+            }
         }
     }
 
-    Context 'When ExtensionType is AddToCertificateAia and there is one AddToCertificateAia URI and one AddToCertificateOcsp URI' {
-        BeforeAll {
-            Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
-                return @(
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $false
-                        AddToCertificateOcsp = $true
-                        Uri                  = 'http://primary-ocsp-responder/ocsp'
-                    },
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $true
-                        AddToCertificateOcsp = $false
-                        Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
-                    }
-                )
+    Context 'When ExtensionType is AddToCertificateOcsp' {
+        Context 'When there is AddToCertificateOcsp URI and one AddToCertificateAia URI' {
+            BeforeAll {
+                Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
+                    return @(
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $false
+                            AddToCertificateOcsp = $true
+                            Uri                  = 'http://primary-ocsp-responder/ocsp'
+                        },
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $true
+                            AddToCertificateOcsp = $false
+                            Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
+                        }
+                    )
+                }
+            }
+
+            It 'Should return the correct result' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+
+                    { $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateOcsp' } | Should -Not -Throw
+
+                    $getCaAiaUriListResult | Should -BeExactly 'http://primary-ocsp-responder/ocsp'
+                }
+
+                Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
             }
         }
 
-        It 'Should return the correct result' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                { $script:result = Get-CaAiaUriList -ExtensionType 'AddToCertificateAia' } | Should -Not -Throw
-                $result | Should -BeExactly 'http://primary/Certs/<CATruncatedName>.cer'
+        Context 'When there is AddToCertificateOcsp URI and two AddToCertificateAia URIs' {
+            BeforeAll {
+                Mock -CommandName Get-CAAuthorityInformationAccess -MockWith {
+                    return @(
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $false
+                            AddToCertificateOcsp = $true
+                            Uri                  = 'http://primary-ocsp-responder/ocsp'
+                        },
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $false
+                            AddToCertificateOcsp = $true
+                            Uri                  = 'http://secondary-ocsp-responder/ocsp'
+                        },
+                        [PSCustomObject] @{
+                            AddToCertificateAia  = $true
+                            AddToCertificateOcsp = $false
+                            Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
+                        }
+                    )
+                }
             }
 
-            Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
-        }
-    }
+            It 'Should return the correct result' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
 
-    Context 'When ExtensionType is AddToCertificateAia and there is two AddToCertificateAia URI and one AddToCertificateOcsp URIs' {
-        BeforeAll {
-            $getCAAuthorityInformationAccessMock = {
-                @(
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $false
-                        AddToCertificateOcsp = $true
-                        Uri                  = 'http://primary-ocsp-responder/ocsp'
-                    },
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $true
-                        AddToCertificateOcsp = $false
-                        Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
-                    },
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $true
-                        AddToCertificateOcsp = $false
-                        Uri                  = 'http://secondary/Certs/<CATruncatedName>.cer'
-                    }
-                )
+                    { $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateOcsp' } | Should -Not -Throw
+
+                    $getCaAiaUriListResult[0] | Should -BeExactly 'http://primary-ocsp-responder/ocsp'
+                    $getCaAiaUriListResult[1] | Should -BeExactly 'http://secondary-ocsp-responder/ocsp'
+                }
+
+                Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope It
             }
-
-            Mock -CommandName Get-CAAuthorityInformationAccess -MockWith $getCAAuthorityInformationAccessMock
-        }
-
-        It 'Should not throw an exception' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                {
-                    $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateAia'
-                } | Should -Not -Throw
-            }
-        }
-
-        It 'Should return two entries' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                $getCaAiaUriListResult[0] | Should -BeExactly 'http://primary/Certs/<CATruncatedName>.cer'
-                $getCaAiaUriListResult[1] | Should -BeExactly 'http://secondary/Certs/<CATruncatedName>.cer'
-            }
-        }
-
-        It 'Should call expected mocks' {
-            Should -Invoke `
-                -CommandName Get-CAAuthorityInformationAccess `
-                -Exactly -Times 1 -Scope Context
-        }
-    }
-
-    Context 'When ExtensionType is AddToCertificateAia and there are only AddToCertificateAia URIs' {
-        BeforeAll {
-            $getCAAuthorityInformationAccessMock = {
-                @(
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $true
-                        AddToCertificateOcsp = $false
-                        Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
-                    }
-                )
-            }
-
-            Mock -CommandName Get-CAAuthorityInformationAccess -MockWith $getCAAuthorityInformationAccessMock
-        }
-
-        It 'Should not throw an exception' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                {
-                    $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateOcsp'
-                } | Should -Not -Throw
-            }
-        }
-
-        It 'Should return null' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                $getCaAiaUriListResult | Should -BeNullOrEmpty
-            }
-        }
-
-        It 'Should call expected mocks' {
-            Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope Context
-        }
-    }
-
-    Context 'When ExtensionType is AddToCertificateOcsp and there is AddToCertificateOcsp URI and one AddToCertificateAia URI' {
-        BeforeAll {
-            $getCAAuthorityInformationAccessMock = {
-                @(
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $false
-                        AddToCertificateOcsp = $true
-                        Uri                  = 'http://primary-ocsp-responder/ocsp'
-                    },
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $true
-                        AddToCertificateOcsp = $false
-                        Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
-                    }
-                )
-            }
-
-            Mock -CommandName Get-CAAuthorityInformationAccess -MockWith $getCAAuthorityInformationAccessMock
-        }
-
-        It 'Should not throw an exception' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                {
-                    $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateOcsp'
-                } | Should -Not -Throw
-            }
-        }
-
-        It 'Should return one OCSP entry' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                $getCaAiaUriListResult | Should -BeExactly 'http://primary-ocsp-responder/ocsp'
-            }
-        }
-
-        It 'Should call expected mocks' {
-            Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope Context
-        }
-    }
-
-    Context 'When ExtensionType is AddToCertificateOcsp and there is AddToCertificateOcsp URI and two AddToCertificateAia URIs' {
-        BeforeAll {
-            $getCAAuthorityInformationAccessMock = {
-                @(
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $false
-                        AddToCertificateOcsp = $true
-                        Uri                  = 'http://primary-ocsp-responder/ocsp'
-                    },
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $false
-                        AddToCertificateOcsp = $true
-                        Uri                  = 'http://secondary-ocsp-responder/ocsp'
-                    },
-                    [PSCustomObject] @{
-                        AddToCertificateAia  = $true
-                        AddToCertificateOcsp = $false
-                        Uri                  = 'http://primary/Certs/<CATruncatedName>.cer'
-                    }
-                )
-            }
-
-            Mock -CommandName Get-CAAuthorityInformationAccess -MockWith $getCAAuthorityInformationAccessMock
-        }
-
-        It 'Should not throw an exception' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                {
-                    $script:getCaAiaUriListResult = Get-CaAiaUriList -ExtensionType 'AddToCertificateOcsp'
-                } | Should -Not -Throw
-            }
-        }
-
-        It 'Should return the correct URIs' {
-            InModuleScope -ScriptBlock {
-                Set-StrictMode -Version 1.0
-
-                $getCaAiaUriListResult[0] | Should -BeExactly 'http://primary-ocsp-responder/ocsp'
-                $getCaAiaUriListResult[1] | Should -BeExactly 'http://secondary-ocsp-responder/ocsp'
-            }
-        }
-
-        It 'Should call expected mocks' {
-            Should -Invoke -CommandName Get-CAAuthorityInformationAccess -Exactly -Times 1 -Scope Context
         }
     }
 }
